21fd41857cc7d864cbd190d7d0df6d11d1f24b5b
Yogeeswar Chekkera <yogeeswarchekkera@gmail.com>
2026-03-14 23:24:25 +0530

    Add HelloApp to greet users based on input

---
+++ UC2.java
@@ -0,0 +1,11 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];  
+            System.out.println("Hello, " + name + "!");
+        } 
+        else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
